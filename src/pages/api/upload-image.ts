c0a9dc4a2c0f084e9ff88889181ea952743b6195
Kibaek Kim <kibaek.kimm@gmail.com>
2023-04-20 01:43:58 +0900

    feat: 이미지 테스트용 api 추가

---
+++ src/pages/api/upload-image.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+import { NextApiRequest, NextApiResponse } from "next";
+import formidable from "formidable";
+
+const uploadDir =
+  "/Users/kibaekkim/Desktop/workspace/site-builder/public/uploads";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const form = new formidable.IncomingForm();
+    form.uploadDir = "./public/uploads";
+    form.keepExtensions = true;
+
+    form.parse(req, (error, fields, files) => {
+      if (error) {
+        res
+          .status(400)
+          .send({ error: "Error parsing the incoming form data." });
+        return;
+      }
+
+      const file = files.image;
+      const newPath = `./public/uploads/${file.name}`;
+      console.log(file);
+
+      fs.renameSync(file.path, newPath);
+
+      res.status(200).send({
+        message: "Image uploaded successfully",
+        path: `/uploads/${file.name}`,
+      });
+    });
+  } else {
+    res.status(405).send({ error: "Only POST method is allowed." });
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default (req, res) => handler(req, res);
