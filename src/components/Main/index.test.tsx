e3f1df95e1696f8f3b3668d2d6a184608794bdef
Kibaek Kim <kibaek.kimm@gmail.com>
2023-04-19 14:44:21 +0900

    feat: Main 섹션 추가

---
+++ src/components/Main/index.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import Main from ".";
+import userEvent from "@testing-library/user-event";
+
+describe("Main", () => {
+  test("메인 텍스트를 변경할 수 있다.", async () => {
+    const user = userEvent.setup();
+    await render(<Main />);
+    const 헤딩1 = screen.getByRole("heading");
+    await user.type(헤딩1, "즐겁게 성잘할 수 있도록");
+
+    expect(헤딩1.innerHTML).toEqual("즐겁게 성잘할 수 있도록");
+  });
+
+  test("defaultValues가 있으면 초기값이 설정된다.", async () => {
+    await render(
+      <Main defaultValues={{ heading: "default value 제목입니다." }} />
+    );
+    const 헤딩1 = screen.getByRole("heading");
+
+    expect(헤딩1.innerHTML).toEqual("default value 제목입니다.");
+  });
+
+  test("값이 변경되면 onChange props를 호출한다.", async () => {
+    const onChange = jest.fn();
+    const user = userEvent.setup();
+    await render(<Main onChange={onChange} />);
+
+    const 헤딩1 = screen.getByRole("heading");
+    await user.type(헤딩1, "즐겁게 성잘할 수 있도록");
+    await user.click(document.body);
+
+    // TODO: backgroundImage 업로드  추가
+
+    expect(onChange).toHaveBeenCalledWith({
+      heading: "즐겁게 성잘할 수 있도록",
+      backgroundImage: "",
+    });
+  });
+
+  test.todo("배경 이미지를 변경할 수 있다.");
+});
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import ContentEditable from "../ContentEditable";
+import styles from "./Main.module.css";
+import { ChangeEvent, useState } from "react";
+import axios from "axios";
+import { MainSectionValues } from "@/types";
+
+interface Props {
+  defaultValues?: Partial<MainSectionValues>;
+  onChange?: (values: MainSectionValues) => void;
+}
+
+export default function Main({ defaultValues, onChange }: Props) {
+  const [values, setValues] = useState<MainSectionValues>({
+    heading: defaultValues?.heading ?? "",
+    backgroundImage: defaultValues?.backgroundImage ?? "",
+  });
+
+  const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const target = e.currentTarget;
+    const file = (target.files as FileList)[0];
+
+    if (file) {
+      const formData = new FormData();
+      formData.append("image", file);
+
+      axios
+        .post("/api/upload-image", formData)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <ContentEditable
+        className={styles.heading}
+        tagName="h1"
+        placeholder="제목을 입력하세요."
+        defaultValue={values.heading}
+        onInputChange={(content) => {
+          const newValues = { ...values, heading: content };
+          setValues(newValues);
+
+          if (onChange) {
+            onChange(newValues);
+          }
+        }}
+      />
+    </div>
+  );
+}
