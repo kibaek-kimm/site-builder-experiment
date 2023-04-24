e8c2bd148ff476dc97ac355c9596b4e3cb4c172f
Kibaek Kim <kibaek.kimm@gmail.com>
2023-04-24 19:12:39 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
-import { Inter } from "next/font/google";
+"use client";
+
 import styles from "./page.module.css";
 import Vision from "@/components/Vision";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useState } from "react";
+import Main from "@/components/Main";
+import { BuilderValues } from "@/types";
+import Welfare from "@/components/Welfare";
 
 export default function Home() {
+  const [builderValues, setBuilderValues] = useState<BuilderValues>({});
+
+  const handleChange = (key, values) => {
+    setBuilderValues((prevState) => ({
+      ...prevState,
+      [key]: values,
+    }));
+  };
+
   return (
     <main className={styles.main}>
-      <Vision />
+      <div className={styles.editArea}>
+        <Main onChange={(values) => handleChange("main", values)} />
+        <Vision onChange={(values) => handleChange("vision", values)} />
+        <Welfare onChange={(values) => handleChange("welfare", values)} />
+      </div>
+      <div className={styles.dataArea}>
+        <pre>{JSON.stringify(builderValues, null, 4)}</pre>
+      </div>
     </main>
   );
 }
