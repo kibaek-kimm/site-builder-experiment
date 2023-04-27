74404c94a5a71f5bf3c5a7a504d810e5e0d5ab66
Kibaek Kim <kibaek.kimm@gmail.com>
2023-04-27 15:44:06 +0900

    fix: 테스트 에러 수정

--- src/features/editor/contents/HighlightCard/index.test.tsx
+++ src/features/editor/contents/HighlightCard/index.test.tsx
@@ -1,5 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import HighlightCard from ".";
+import userEvent from "@testing-library/user-event";
+
 describe("HighlightCard", () => {
-  test.todo("제목을 수정할 수 있다.");
+  test("제목을 수정할 수 있다.", async () => {
+    render(<HighlightCard />);
+
+    const 헤딩리스트 = screen.getAllByRole("heading");
+    const 메인헤딩 = 헤딩리스트.find(
+      ({ tagName }) => tagName === "H2"
+    ) as HTMLElement;
+    await userEvent.type(
+      메인헤딩,
+      "원티드는 오늘도 일의 미래를 만들어가고 있습니다"
+    );
+
+    expect(메인헤딩.innerHTML).toEqual(
+      "원티드는 오늘도 일의 미래를 만들어가고 있습니다"
+    );
+  });
   test.todo("카드를 수정할 수 있다.");
   test.todo("카드 개수를 설정 할 수 있다.");
   test.todo(
