ae1548e9d0e47e2cd06d14ca8b5c884b9ae49474
Kibaek Kim <kibaek.kimm@gmail.com>
2023-05-03 00:22:12 +0900

    fix: Footer 컴포넌트 editable하지않도록 수정

--- src/features/editor/contents/Footer/index.tsx
+++ src/features/editor/contents/Footer/index.tsx
@@ -1,44 +1,16 @@
 import { FooterValues } from "@/types";
 import styles from "./Footer.module.css";
-import { useState } from "react";
-import ContentEditable from "../../core/ContentEditable";
 
-interface Props {
-  defaultValue?: FooterValues;
-  onChange?: (values: FooterValues) => void;
-}
+interface Props extends FooterValues {}
 
-export default function Footer({ defaultValue, onChange }: Props) {
-  const [values, setValues] = useState<FooterValues>(defaultValue ?? {});
-
-  const handleChangeValues = (key: string, value: string) => {
-    const newState = { ...values };
-    newState[key] = value;
-
-    setValues(newState);
-
-    if (onChange) {
-      onChange(newState);
-    }
-  };
-
+export default function Footer({ companyName, companyInfo }: Props) {
   return (
     <div className={styles.wrapper}>
-      <ContentEditable
-        tagName="h2"
-        className={styles.footerTitle}
-        defaultValue={values.companyName}
-        onInputChange={(content) => handleChangeValues("companyName", content)}
-      />
+      <h2 className={styles.footerTitle}>{companyName}</h2>
       <div>
-        <ContentEditable
-          tagName="address"
+        <address
           className={styles.address}
-          defaultValue={values.companyInfo}
-          role="company-information"
-          onInputChange={(content) =>
-            handleChangeValues("companyInfo", content)
-          }
+          dangerouslySetInnerHTML={{ __html: companyInfo }}
         />
       </div>
     </div>
