b84b031d8c15f60443a52670ba31b2b73cbc6ccd
Kibaek Kim <kibaek.kimm@gmail.com>
2023-05-03 19:42:03 +0900

    feat: 미리보기 링크 추가

--- src/features/layout/GlobalNavigation/index.tsx
+++ src/features/layout/GlobalNavigation/index.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
 import styles from "./GlobalNavigation.module.css";
 
 export default function GlobalNavigation() {
-  return <header className={styles.gnb}></header>;
+  return (
+    <header className={styles.gnb}>
+      <Link href="/preview" target="_blank">
+        <button>미리보기</button>
+      </Link>
+    </header>
+  );
 }
